954deb2f2c0b22ef2171ce30e1f023986f7f6b35
Vab31 <97352743+Vab31@users.noreply.github.com>
2023-05-14 03:10:13 +0530

    Update home.js

--- src/component/home.js
+++ src/component/home.js
@@ -10,7 +10,7 @@ export default function Home() {
 
     <div className="h-screen  justify-center items-center">
             
-            <p className="text-5xl text-amber-300 lg:-ml-96 ">
+            <p className="text-3xl text-amber-300 lg:-ml-96 ">
              Navigation  which have some dummy button which are not working
               only  ...<b>"working section"</b>... of Navigation is working
               <br/>
